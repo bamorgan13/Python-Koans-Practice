46714d704a18e7b8caf6490dcc3ae5af0b8d0a3b
Bryce Morgan <bamorgan13@gmail.com>
2019-11-25 22:37:26 -0800

    Complete about_decorating_with_classes

--- python3/koans/about_decorating_with_classes.py
+++ python3/koans/about_decorating_with_classes.py
@@ -5,9 +5,10 @@ from runner.koan import *
 
 import functools
 
+
 class AboutDecoratingWithClasses(Koan):
     def maximum(self, a, b):
-        if a>b:
+        if a > b:
             return a
         else:
             return b
@@ -19,21 +20,21 @@ class AboutDecoratingWithClasses(Koan):
         """
         max = functools.partial(self.maximum)
 
-        self.assertEqual(__, max(7,23))
-        self.assertEqual(__, max(10,-10))
+        self.assertEqual(23, max(7, 23))
+        self.assertEqual(10, max(10, -10))
 
     def test_partial_that_wrappers_first_arg(self):
         max0 = functools.partial(self.maximum, 0)
 
-        self.assertEqual(__, max0(-4))
-        self.assertEqual(__, max0(5))
+        self.assertEqual(0, max0(-4))
+        self.assertEqual(5, max0(5))
 
     def test_partial_that_wrappers_all_args(self):
         always99 = functools.partial(self.maximum, 99, 20)
         always20 = functools.partial(self.maximum, 9, 20)
 
-        self.assertEqual(__, always99())
-        self.assertEqual(__, always20())
+        self.assertEqual(99, always99())
+        self.assertEqual(20, always20())
 
     # ------------------------------------------------------------------
 
@@ -64,17 +65,18 @@ class AboutDecoratingWithClasses(Koan):
         # To clarify: the decorator above the function has no arguments, even
         # if the decorated function does
 
-        self.assertEqual(__, self.foo())
-        self.assertEqual(__, self.parrot('pieces of eight'))
+        self.assertEqual('foo, foo', self.foo())
+        self.assertEqual('PIECES OF EIGHT, PIECES OF EIGHT',
+                         self.parrot('pieces of eight'))
 
     # ------------------------------------------------------------------
 
     def sound_check(self):
-        #Note: no decorator
+        # Note: no decorator
         return "Testing..."
 
     def test_what_a_decorator_is_doing_to_a_function(self):
-        #wrap the function with the decorator
+        # wrap the function with the decorator
         self.sound_check = self.doubleit(self.sound_check)
 
         self.assertEqual(__, self.sound_check())
@@ -98,10 +100,11 @@ class AboutDecoratingWithClasses(Koan):
     @documenter("Increments a value by one. Kind of.")
     def count_badly(self, num):
         num += 1
-        if num==3:
+        if num == 3:
             return 5
         else:
             return num
+
     @documenter("Does nothing")
     def idler(self, num):
         "Idler"
@@ -125,4 +128,3 @@ class AboutDecoratingWithClasses(Koan):
     def test_we_can_chain_decorators(self):
         self.assertEqual(__, self.homer())
         self.assertEqual(__, self.homer.__doc__)
-
